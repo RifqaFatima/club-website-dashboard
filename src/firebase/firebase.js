9af2e87f906b06d1f0e20c0806ff42db390610b0
RifqaFatima <rifqa88077@gmail.com>
2026-01-05 16:20:52 +0530

    Phase 1: initial React + Firebase setup

---
+++ src/firebase/firebase.js
@@ -0,0 +1,20 @@
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBEUy7d9OcPTyhRWHeu3HirjyJ-JRprPeE",
+  authDomain: "ieee-cs-p4-phase1.firebaseapp.com",
+  projectId: "ieee-cs-p4-phase1",
+  storageBucket: "ieee-cs-p4-phase1.firebasestorage.app",
+  messagingSenderId: "815356482844",
+  appId: "1:815356482844:web:01431b4fe879f324e056d9"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+const db = getFirestore(app);
+
+export { auth, db };
